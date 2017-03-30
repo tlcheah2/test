9a70c2b985769a2a8277219d104b0893b406eb49
Tek Loon <tekloon.1991@gmail.com>
2017-03-30 12:39:57 +0800

    Fixed child view won't render
    Added measureAndLayout when requestLayout called

--- android/app/src/main/java/com/test/TestContainerView.java
+++ android/app/src/main/java/com/test/TestContainerView.java
@@ -49,5 +49,21 @@ public class TestContainerView extends FrameLayout {
         this.addView(viewPager);
     }
 
+    @Override
+    public void requestLayout() {
+        super.requestLayout();
+        post(measureAndLayout);
+    }
+
+    private final Runnable measureAndLayout = new Runnable() {
+        @Override
+        public void run() {
+            measure(
+                    MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.EXACTLY));
+            layout(getLeft(), getTop(), getRight(), getBottom());
+        }
+    };
+
 
 }
